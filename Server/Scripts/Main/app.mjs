6d218f1568ba4701c6db0aa5e55b877fd6d9e614
ankitchemjong <npp.rasik@gmail.com>
2025-09-04 17:13:58 +0545

    cc

--- Server/Scripts/Main/app.mjs
+++ Server/Scripts/Main/app.mjs
@@ -54,8 +54,8 @@ const main = () => {
       cookie: {
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true,
-        secure: process.env.NODE_ENV == 'production',
-        sameSite: process.env.NODE_ENV == 'production' ? 'none' : 'lax',
+        secure:true,
+        sameSite:'none' // 'lax',
       },
       name: "hook",
     })
